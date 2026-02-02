a4ba5a0e8eb134acf0485a939e0fe1cb33ad6583
AlexandrosPrps92 <124439066+AlexandrosPrps92@users.noreply.github.com>
2026-02-02 12:22:40 +0200

    change in background

--- src/App.jsx
+++ src/App.jsx
@@ -308,7 +308,7 @@ const Contact = () => {
                     <a href="www.linkedin.com/in/alexandros-prepis-792796163" className="p-4 rounded-full bg-zinc-900 hover:bg-zinc-800 transition-colors">
                         <Linkedin className="w-6 h-6 text-blue-500" />
                     </a>
-                    <a href="#" className="p-4 rounded-full bg-zinc-900 hover:bg-zinc-800 transition-colors">
+                    <a href="https://github.com/AlexandrosPrps92" className="p-4 rounded-full bg-zinc-900 hover:bg-zinc-800 transition-colors">
                         <Github className="w-6 h-6 text-white" />
                     </a>
                 </div>
