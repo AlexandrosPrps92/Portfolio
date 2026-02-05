c6eaadb2a2af6809629a5accbb91b5dc0c093f7d
AlexandrosPrps92 <124439066+AlexandrosPrps92@users.noreply.github.com>
2026-02-05 13:33:53 +0200

    youtube_blockquote

--- src/App.jsx
+++ src/App.jsx
@@ -609,16 +609,24 @@ const PROJECTS = [
                         id: "f1",
                         title: "Album & Video Covers",
                         category: "Music Videos",
-                        src: "/videos/freelance_reel.mp4",
+                        src: "/videos/quaresma.mp4",
                         className: "md:col-span-2 md:row-span-2"
                     },
                     {
                         id: "f2",
-                        title: "LinkedIn Feature",
+                        title: "Youtube Feature",
                         category: "Embedded Post",
                         embed: `<iframe width="100%" height="100%" src="https://www.youtube.com/embed/videoseries?si=WyYGssk8WaSLG1ul&amp;list=PLKlubVxCs3I5UdoJRvUzh2elMD1kR80iP" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
                         className: "md:col-span-1 md:row-span-2"
                     },
+                    {
+                        id: "f1",
+                        title: "Album & Video Covers",
+                        category: "Music Videos",
+                        src: "/videos/panselinos.mp4",
+                        className: "md:col-span-2 md:row-span-2"
+                    },
+
                 ]
             }
         ]
